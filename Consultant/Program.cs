7e864fbf71c065d34d43714e4d1210313d28a436
mmdrz1988 <m.r.sasani1988@gmail.com>
2024-05-10 21:28:31 +0330

    Add project files.

---
+++ Consultant/Program.cs
@@ -0,0 +1,213 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Text = DocumentFormat.OpenXml.Wordprocessing.Text;
+
+namespace Consultant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World! hhh");
+            SaveWordProcessingDocument();
+        }
+        public static void SaveWordProcessingDocument()
+        {
+            string[] mybookmark = 
+            {
+                "From" ,"To","Content"
+            };
+
+            byte[] byteArray = File.ReadAllBytes("C:\\Users\\mmdr.sasani\\source\\repos\\LetterTemplate\\Consultant\\content\\test1.docx");
+            using (var stream = new MemoryStream())
+            {
+                stream.Write(byteArray, 0, byteArray.Length);
+                using (var wordDoc = WordprocessingDocument.Open(stream, true))
+                {
+                    // Do work here
+                    var mainPart = wordDoc.MainDocumentPart;
+                    if (mainPart != null)
+                    {
+                        var bookmarks = mainPart.Document.Body.Descendants<BookmarkStart>();
+                        if (!bookmarks.Any())
+                        {
+                            Console.WriteLine("No bookmarks found in the document.");
+                        }
+                        else
+                        {
+                            IDictionary<String, BookmarkStart> bookmarkMap =
+                                    new Dictionary<String, BookmarkStart>();
+                            foreach (var bookmarkStart in bookmarks)
+                            {
+                                Console.WriteLine($"Bookmark Name: {bookmarkStart.Name}");
+                                bookmarkMap[bookmarkStart.Name] = bookmarkStart;
+                            }
+                            foreach (BookmarkStart bookmarkStart in bookmarkMap.Values)
+                            {
+                                Run bookmarkText = bookmarkStart.NextSibling<Run>();
+
+                                foreach (var mb in mybookmark)
+                                {
+                                    if (bookmarkStart.Name == mb)
+                                    {
+                                        Console.WriteLine($"It's correctly : {bookmarkStart.Name}");
+
+                                    }
+
+                                }
+                                if (bookmarkStart.Name == "From")
+                                {
+                                    Console.WriteLine($"Bookmark From replacement: {bookmarkStart.Name}");
+                                    if (bookmarkText != null)
+                                    {
+                                        bookmarkText.GetFirstChild<Text>().Text = "امیر محمد رضایی";
+                                    }
+                                }
+                                else if (bookmarkStart.Name == "To")
+                                {
+                                    if (bookmarkText != null)
+                                    {
+                                        bookmarkText.GetFirstChild<Text>().Text = "محمدرضا ساسانی";
+                                    }
+                                }
+                                else if (bookmarkStart.Name == "Content")
+                                {
+                                    if (bookmarkText != null)
+                                    {
+                                        bookmarkText.GetFirstChild<Text>().Text = "گوگل و محتوای کپی و تکراری، مثل کارد و پنیرند و به‌هیچ‌وجه آب‌شان به یک جوب نمی‌رود!\r\n\r\nمانند سایر موتورهای جست‌وجو، گوگل هم نمی‌خواهد ۱۰ محتوای یکسان و مشابه را در صفحهٔ نتایج جست‌وجو به کاربر ارائه دهد؛ پس سعی می‌کند محتواهای منحصربه‌فرد را شناسایی کند و به آن‌ها احترام بگذارد.\r\n\r\nوقتی گوگل با چند محتوای کپی مواجه شود، بین آن‌ها گیر می‌کند و نمی‌تواند بفهمد کدام را به کاربر پیشنهاد دهد و اعتبار لینک‌ها و رتبهٔ بهتر را باید به کدام یکی اعطا کند. این اتفاق به‌شدت به رتبه‌بندی سایت شما آسیب می‌زند.\r\n\r\nبرای اینکه پول خودتان را خرج مقالهٔ کپی‌شده نکنید و مخاطبان خود را به‌خاطر آن از دست دهید، بهتر است به سراغ ابزارهای تشخیص محتوای کپی بروید.\r\n\r\nقبل از اینکه ۱۰ ابزار کاربردی و مناسب برای محتواهای فارسی و انگلیسی را به شما معرفی کنیم، بیایید ببینیم اصلاً به چه چیزی محتوای کپی می‌گوییم!";
+                                    }
+                                }
+                                else
+                                {
+                                    if (bookmarkText != null)
+                                    {
+                                        bookmarkText.GetFirstChild<Text>().Text =" ";
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                    // ...
+                    //GetBookmarksFromDocx(byteArray);
+                    wordDoc.MainDocumentPart.Document.Save(); // won't update the original file 
+                }
+                // Save the file with the new name
+                stream.Position = 0;
+                Guid guid = Guid.NewGuid();
+                File.WriteAllBytes($@"C:\Users\mmdr.sasani\source\repos\LetterTemplate\Consultant\content\Letter\{guid}.docx", stream.ToArray());
+            }
+        }
+        //------------------------------------------------------------------------
+        //----------------------------------------------------------------------
+        public static void GetBookmarksFromDocx(string filePath)
+        {
+            //string strDoc = @"C:\Users\mmdr.sasani\source\repos\LetterTemplate\Consultant\content\test.docx";
+            //GetBookmarksFromDocx(strDoc);
+            //string strDoc = @"E:\resulttest.docx";
+            //string txt = "Append text in body - OpenAndAddToWordprocessingStream";
+            //Stream stream = File.Open(strDoc, FileMode.Open);
+            //OpenAndAddToWordprocessingStream(stream, txt);
+            //stream.Close();
+            //----------------------------------------------------
+            //GetBookmarksFromDocx("E:\\test.docx");
+            //OpenWordprocessingDocumentReadonly(@"E:\WordDocTest\Test.docx");
+            try
+            {
+                using var wordDoc = WordprocessingDocument.Open(filePath, false);
+                var mainPart = wordDoc.MainDocumentPart;
+                if (mainPart != null)
+                {
+                    var bookmarks = mainPart.Document.Body.Descendants<BookmarkStart>();
+                    if (!bookmarks.Any())
+                    {
+                        Console.WriteLine("No bookmarks found in the document.");
+                    }
+                    else
+                    {
+                        foreach (var bookmarkStart in bookmarks)
+                        {
+                            Console.WriteLine($"Bookmark Name: {bookmarkStart.Name}");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No bookmarks found in the document.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        //-----------------------------------------------------------------------
+        static void OpenAndAddToWordprocessingStream(Stream stream, string txt)
+        {
+            // Open a WordProcessingDocument based on a stream.
+            WordprocessingDocument wordprocessingDocument = WordprocessingDocument.Open(stream, true);
+
+            // Assign a reference to the existing document body.
+            MainDocumentPart mainDocumentPart = wordprocessingDocument.MainDocumentPart ?? wordprocessingDocument.AddMainDocumentPart();
+            mainDocumentPart.Document ??= new Document();
+            Body body = mainDocumentPart.Document.Body ?? mainDocumentPart.Document.AppendChild(new Body());
+
+            // Add new text.
+            Paragraph para = body.AppendChild(new Paragraph());
+            Run run = para.AppendChild(new Run());
+            run.AppendChild(new DocumentFormat.OpenXml.Drawing.Text(txt));
+
+            // Dispose the document handle.
+            wordprocessingDocument.Dispose();
+
+            // Caller must close the stream.
+
+
+            //  save to another file 
+
+        }
+        //------------------------------------------------------------------------
+
+        public static void OpenWordprocessingDocumentReadonly(string filepath)
+        {
+            // Open a WordprocessingDocument based on a filepath.
+            using (WordprocessingDocument wordDocument =
+                WordprocessingDocument.Open(filepath, false))
+            {
+                // Assign a reference to the existing document body.  
+                Body body = wordDocument.MainDocumentPart.Document.Body;
+                //var paras = body.OfType<Paragraph>();
+                var paras = body.OfType<Paragraph>()
+                            .Where(p => p.ParagraphProperties != null &&
+                            p.ParagraphProperties.ParagraphStyleId != null &&
+                            p.ParagraphProperties.ParagraphStyleId.Val.Value.Contains("Heading1")).ToList();
+                foreach (var para in paras)
+                {
+                    //richTextBox1.Text += para.NextSibling().InnerText + "\n";
+                }
+                Console.WriteLine(body.InnerText);
+                //wordDocument.Close();
+            }
+        }        
+    }
+}
+//IDictionary<String, BookmarkStart> bookmarkMap =
+//    new Dictionary<String, BookmarkStart>();
+
+//foreach (BookmarkStart bookmarkStart in file.MainDocumentPart.RootElement.Descendants<BookmarkStart>())
+//{
+//    bookmarkMap[bookmarkStart.Name] = bookmarkStart;
+//}
+
+//foreach (BookmarkStart bookmarkStart in bookmarkMap.Values)
+//{
+//    Run bookmarkText = bookmarkStart.NextSibling<Run>();
+//    if (bookmarkText != null)
+//    {
+//        bookmarkText.GetFirstChild<Text>().Text = "blah";
+//    }
+//}
---
+++ LetterTemplate/Program.cs
@@ -0,0 +1,190 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SautinSoft.Document;
+using SautinSoft.Document.Drawing;
+using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
+
+namespace LetterTemplate
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+
+            string ResultPath = @"E:\result.docx";
+            string loadPath = @"E:\result.docx";
+            string ExistText = "SautinSoft.Document 2024.4.24 trial. Get your free 30-day key instantly: https://sautinsoft.com/start-for-free/";
+
+            string SavePdfPath = @"E:\replaced.pdf";
+            string SavePdfPath2 = @"E:\";
+            string picPath = @"E:\12.png";
+            //string SavePdfPath = @"E:\Replaced_signature.pdf";
+
+            //DocumentCore dc = new DocumentCore();
+            //DocumentBuilder db = new DocumentBuilder(dc);
+            //db.StartBookmark("BookMark1");
+            //db.Write("This is <BookMark> 1 and this is signature <signature>");
+            //db.EndBookmark("BookMark1");
+            //db.InsertSpecialCharacter(SpecialCharacterType.LineBreak);
+            //dc.Save(ResultPath);
+
+
+            //FindTextAndReplaceAnyText(loadPath, "BookMark", "Target Text", SavePdfPath);
+            //FindTextAndReplaceImage(loadPath, picPath, SavePdfPath2);
+            HeadersAndFooters(null);
+            //FindTextAndReplaceAnyText(loadPath, ExistText, "s", SavePdfPath, true);
+        }
+        public static void FindTextAndReplaceAnyText(string loadPath, string ExistText, string TargetText, string SavePdfPath, bool IsCopyRightContent=false)
+        {
+            DocumentCore dc = DocumentCore.Load(loadPath);
+            Regex regex = new Regex(@$"{ExistText}", RegexOptions.IgnoreCase);
+            //Find "Bean" and Replace everywhere to "Joker"
+
+            foreach (ContentRange item in dc.Content.Find(regex))
+            {
+                if (IsCopyRightContent)
+                {
+                    item.Delete();
+                }
+                else
+                {
+                    item.Replace(@$"{TargetText}");
+                }
+                
+            }
+
+            string savePath = Path.ChangeExtension(SavePdfPath ,".replaced.pdf");
+            dc.Save(loadPath);
+            //dc.Save(loadPath, new PdfSaveOptions());
+            //System.Diagnostics.Process.Start(savePath);
+        }
+
+        public static void FindTextAndReplaceImage(string loadPath, string picPath , string SavePdfPath)
+        {
+
+            // Load a document intoDocumentCore.
+            DocumentCore dc = DocumentCore.Load(loadPath);
+
+            //Find "<signature>" Text and Replace everywhere with the "Smile.png"
+            // Please note, Reverse() makes sure that action replace not affects to Find().
+            Regex regex = new Regex(@"<signature>", RegexOptions.IgnoreCase);
+            Picture picture = new Picture(dc, InlineLayout.Inline(new Size(50, 50)), picPath);
+            foreach (ContentRange item in dc.Content.Find(regex).Reverse())
+            {
+                item.Replace(picture.Content);
+            }
+
+            // Save our document into PDF format.
+            dc.Save(loadPath);
+            //dc.Save(SavePdfPath, new PdfSaveOptions());
+
+            // Open the result for demonstration purposes.
+            //System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(loadPath) { UseShellExecute = true });
+            //System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(SavePdfPath) { UseShellExecute = true });
+        }
+
+        public static void HeadersAndFooters(string loadPath)
+        {
+            // distroy priview error
+            string ExistText = "SautinSoft.Document 2024.4.24 trial. Get your free 30-day key instantly: https://sautinsoft.com/start-for-free/";
+
+            //string documentPath = @"HeadersAndFooters.docx";
+            string documentPath = @"E:\result.docx";
+            // Let's create a simple document.
+            //DocumentCore dc = DocumentCore.Load(documentPath);
+            DocumentCore dc = new DocumentCore();
+            // Add a new section in the document.
+            Section s = new Section(dc);
+            dc.Sections.Add(s);
+
+            // Let's add a paragraph with text.
+            Paragraph p = new Paragraph(dc);
+            dc.Sections[0].Blocks.Add(p);
+
+            p.ParagraphFormat.Alignment = HorizontalAlignment.Justify;
+            p.Content.Start.Insert("سلام چطوری این پیام به صورت آزمایشی ایجاد گردیده است " +
+                                   "شما می توانید از ساهتار این متن استفاده و نامه خود را ارسال نمایید شماره 894839 مربوط به نامه می باشد", new CharacterFormat() { Size = 12, FontName = "Arial" });
+            p.ParagraphFormat.Alignment = HorizontalAlignment.Right;
+            //p.ParagraphFormat.Alignment = ParagraphAlignment.Left;
+            // Create a new header with formatted text.
+            HeaderFooter header = new HeaderFooter(dc, HeaderFooterType.HeaderDefault);
+            header.Content.Start.Insert("Shrek and Donkey travel to the castle and split up to find Fiona.", new CharacterFormat() { Size = 14.0 });
+
+            // Add the header into HeadersFooters collection of the 1st section.
+            s.HeadersFooters.Add(header);
+
+            // Create a new footer with some text and image.
+            HeaderFooter footer = new HeaderFooter(dc, HeaderFooterType.FooterDefault);
+
+            // Create a paragraph to insert it into the footer.
+            Paragraph par = new Paragraph(dc);
+            par.Content.Start.Insert("Shrek and Donkey travel to the castle and split up to find Fiona. ", new CharacterFormat() { Size = 14.0 });
+            par.ParagraphFormat.Alignment = HorizontalAlignment.Left;
+
+            // Insert image into the paragraph.
+            double wPt = LengthUnitConverter.Convert(2, LengthUnit.Centimeter, LengthUnit.Point);
+            double hPt = LengthUnitConverter.Convert(2, LengthUnit.Centimeter, LengthUnit.Point);
+
+            Picture pict = new Picture(dc, Layout.Inline(new Size(wPt, hPt)), @"E:\12.jpg");
+            par.Inlines.Add(pict);
+
+            // Add the paragraph into the Blocks collection of the footer.
+            footer.Blocks.Add(par);
+
+            // Finally, add the footer into 1st section (HeadersFooters collection).
+            s.HeadersFooters.Add(footer);
+            dc.Save(documentPath);
+
+            // Save the document into DOCX format.
+            FindTextAndReplaceAnyText(documentPath, ExistText, " ", "", true);
+
+
+
+            // Open the result for demonstration purposes.
+            //System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(documentPath) { UseShellExecute = true });
+        }
+
+
+
+
+
+        //static void Main(string[] args)
+        //{
+
+        //    string ResultPath = @"E:\result.docx";
+        //    DocumentCore dc = new DocumentCore();
+        //    DocumentBuilder db = new DocumentBuilder(dc);
+        //    db.StartBookmark("BookMark1");
+        //    db.Write("This is <BookMark> 1");
+        //    db.EndBookmark("BookMark1");
+        //    db.InsertSpecialCharacter(SpecialCharacterType.LineBreak);
+        //    dc.Save(ResultPath);
+
+        //    string loadPath = @"E:\result.docx";
+        //    string picPath = @"E:\12.png";
+        //    string SavePdfPath = @"E:\Replaced_signature.pdf";
+        //    //// Load a document intoDocumentCore.
+        //    //DocumentCore doc = DocumentCore.Load(loadPath);
+
+        //    ////Find "<signature>" Text and Replace everywhere with the "Smile.png"
+        //    //// Please note, Reverse() makes sure that action replace not affects to Find().
+        //    //Regex regex = new Regex(@"<BookMark>", RegexOptions.IgnoreCase);
+        //    //Picture picture = new Picture(doc, InlineLayout.Inline(new Size(50, 50)), pictPath);
+        //    //foreach (ContentRange item in doc.Content.Find(regex).Reverse())
+        //    //{
+        //    //    item.Replace(picture.Content);
+        //    //}
+
+        //    //// Save our document into PDF format.
+        //    //string savePath = @"E:\Replaced_signature.pdf";
+        //    //doc.Save(savePath, new PdfSaveOptions());
+        //    //// Open the result for demonstration purposes.
+        //    //System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(loadPath) { UseShellExecute = true });
+        //    FindTextAndReplaceImage(loadPath, picPath, SavePdfPath);
+
+        //}
+    }
+}
+
+
